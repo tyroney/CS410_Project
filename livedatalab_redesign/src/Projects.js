a1f6b51fb9b8b26cfc4e513e890615232a9f6bdc
collin-fogarty <fogarty4@illinois.edu>
2023-11-19 17:57:48 -0600

    Projects page updates

--- livedatalab_redesign/src/Projects.js
+++ livedatalab_redesign/src/Projects.js
@@ -4,12 +4,25 @@ import './Projects.css';
 function Projects() {
   return (
     <div className="Projects">
+
+      <nav className='NavBar' role='navigation'>
+
+        <div className='Pages'>
+          <img src={logo} className="LiveDataLabLogo" alt="logo" />
+          <a class="navbar-item" href="/">Home</a>
+          <a class="navbar-item" href="/projects">Projects</a>
+          <a class="navbar-item" href="/courses">Courses</a>
+          <a class="navbar-item" href="/manage">Manage</a>
+          <a class="navbar-item" href="/create">Create</a>
+        </div>
+
+      </nav>
+
       <header className="Header">
-        <img src={logo} className="LiveDataLabLogo" alt="logo" />
-        <p className='BodyText'>
-          Your Projects
-        </p>
+        <p>Your Projects</p>
+        <div className='subtext'>No projects started</div>
       </header>
+
     </div>
   );
 }
